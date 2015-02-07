92590941fa27602144fcd76cbac430758e9b1b41
Leejay Schmidt <leejayschmidt@uofs-10-227-3-169.usask.ca>
2015-02-07 15:54:04 -0600

    -Added new view controller for selecting options -Added segue to view controller

---
+++ VAPMON/VAPMON/UIViewController+SelectorViewController.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+SelectorViewController.h
+//  VAPMON
+//
+//  Created by Leejay Schmidt on 2015-02-07.
+//  Copyright (c) 2015 Leejay Schmidt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (SelectorViewController)
+
+@end
---
+++ VAPMON/VAPMON/UIViewController+SelectorViewController.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+SelectorViewController.m
+//  VAPMON
+//
+//  Created by Leejay Schmidt on 2015-02-07.
+//  Copyright (c) 2015 Leejay Schmidt. All rights reserved.
+//
+
+#import "UIViewController+SelectorViewController.h"
+
+@implementation UIViewController (SelectorViewController)
+
+@end
