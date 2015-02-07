57c5aa888605df1e9f7cbf4f8ade2b1a6c7b975f
Leejay Schmidt <leejayschmidt@uofs-10-227-3-169.usask.ca>
2015-02-07 15:44:14 -0600

    -Added the doctor code handler on first page

--- VAPMON/VAPMON/ViewController.m
+++ VAPMON/VAPMON/ViewController.m
@@ -13,15 +13,24 @@
 @end
 
 @implementation ViewController
+@synthesize docCode;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [docCode addTarget:self action:@selector(docCodeChanged) forControlEvents:UIControlEventEditingChanged];
+    [docCode becomeFirstResponder];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)docCodeChanged {
+    if(docCode.text.length == 4) {
+        NSLog(@"Hit the value");
+    }
 }
 
 @end
