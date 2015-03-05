ae1bf4922196e7f7f921f59c4127842b23668fce
Leejay Schmidt <leejayschmidt@leejay-schmidts-macbook-pro-3.local>
2015-03-04 22:25:51 -0600

    -Added data point data to testing

--- VAPMON/VAPMON/ViewController.m
+++ VAPMON/VAPMON/ViewController.m
@@ -60,7 +60,8 @@
             }
             
         }
-        csv = @"patientName|doctorCode|patientNumber\nJimmy Doe|4534|ae5786\nJane Doxall|1234|ae5493";
+        csv = @"patientName|doctorCode|patientNumber\nJimmy Doe|1234|07889879\nJane Doxall|1234|3048de16\n\
+        John Parsons|1234|e7232376";
         data = [parser parseCSV:csv withSeparator:@"|"];
         NSLog(@"%@", data);
         for (NSUInteger i = 0; i < data.count; ++i) {
@@ -71,6 +72,64 @@
             [object setValue:[curPatient valueForKey:@"patientName"] forKey:@"patientName"];
             [object setValue:[curPatient valueForKey:@"doctorCode"] forKey:@"doctorCode"];
             [object setValue:[curPatient valueForKey:@"patientNumber"] forKey:@"patientNumber"];
+            if (![context save:&error]) {
+                NSLog(@"Failed to save - error: %@", [error localizedDescription]);
+            }
+            
+        }
+        csv = @"doctorCode,patientNumber,date,flowRateValue,pressureValue\n\
+        1234,3048de16,2015-02-24,5,1.3\n\
+        1234,3048de16,2015-02-25,5,1.3\n\
+        1234,07889879,2015-02-25,5,1.3\n\
+        1234,07889879,2015-02-25,5,1.3\n\
+        1234,07889879,2015-02-26,5,1.3\n\
+        1234,07889879,2015-02-26,5,1.3\n\
+        1234,07889879,2015-02-26,5,1.3\n\
+        1234,07889879,2015-02-26,5,1.3\n\
+        1234,07889879,2015-02-26,5,1.3\n\
+        1234,07889879,2015-02-26,5,1.3\n\
+        1234,3048de16,2015-03-01,5,1.3\n\
+        1234,3048de16,2015-03-01,5,1.3\n\
+        1234,3048de16,2015-03-01,5,3\n\
+        1234,3048de16,2015-03-01,0,4\n\
+        1234,3048de16,2015-03-01,0,4\n\
+        1234,3048de16,2015-03-01,0,4\n\
+        1234,3048de16,2015-03-01,3.7898912163441487,2.5337436289322595\n\
+        1234,3048de16,2015-03-01,3.7898912163441487,2.5337436289322595\n\
+        1234,3048de16,2015-03-01,3.7898912163441487,2.5337436289322595\n\
+        1234,3048de16,2015-03-01,3,2\n\
+        1234,3048de16,2015-03-01,3,2\n\
+        1234,3048de16,2015-03-01,3,2\n\
+        1234,3048de16,2015-03-01,9,3\n\
+        1234,3048de16,2015-03-01,9,3\n\
+        1234,3048de16,2015-03-01,0,4\n\
+        1234,3048de16,2015-03-01,0,4\n\
+        1234,3048de16,2015-03-01,0,4\n\
+        1234,3048de16,2015-03-01,0,1\n\
+        1234,3048de16,2015-03-01,8,4\n\
+        1234,3048de16,2015-03-01,4,3\n\
+        1234,3048de16,2015-03-01,8,1\n\
+        1234,3048de16,2015-03-01,5,2\n\
+        1234,a7889879,2015-03-01,7,1\n\
+        1234,a7889879,2015-03-01,8,1\n\
+        1234,07889879,2015-03-02,8,3\n\
+        1234,e7232376,2015-03-03,7,1";
+        
+        data = [parser parseCSV:csv withSeparator:@","];
+        NSLog(@"%@", data);
+        for (NSUInteger i = 0; i < data.count; ++i) {
+            object = [NSEntityDescription insertNewObjectForEntityForName:@"DataPoint"
+                                                   inManagedObjectContext:context];
+            
+            NSDictionary *curDP = [data objectAtIndex: i];
+            NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+            NSNumberFormatter *numFormatter = [[NSNumberFormatter alloc] init];
+            [formatter setDateFormat:@"yyyy-MM-dd"];
+            [object setValue:[curDP valueForKey:@"patientNumber"] forKey:@"patientNumber"];
+            [object setValue:[curDP valueForKey:@"doctorCode"] forKey:@"doctorCode"];
+            [object setValue:[formatter dateFromString:[curDP valueForKey:@"date"]] forKey:@"date"];
+            [object setValue:[numFormatter numberFromString:[curDP valueForKey:@"flowRateValue"]] forKey:@"flowRateValue"];
+            [object setValue:[numFormatter numberFromString:[curDP valueForKey:@"pressureValue"]] forKey:@"pressureValue"];
             if (![context save:&error]) {
                 NSLog(@"Failed to save - error: %@", [error localizedDescription]);
             }
